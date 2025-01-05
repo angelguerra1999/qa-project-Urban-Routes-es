cc49158cd3bcd04f709233cee2e253c813120595
angelguerra1999 <134436845+angelguerra1999@users.noreply.github.com>
2025-01-05 13:37:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -19,55 +19,43 @@ class TestUrbanRoutes:
         cls.routes_page = urban_routes_page.UrbanRoutesPage(cls.driver)
 
     def test_set_route(self):
-        self.routes_page.setup_route(data.address_from, data.address_to)
+        self.routes_page.setup_route()
         assert self.routes_page.get_from() == data.address_from
         assert self.routes_page.get_to() == data.address_to
 
     def test_select_comfort_tariff(self):
-        self.routes_page.setup_route(data.address_from, data.address_to)
+        self.routes_page.setup_route()
         self.routes_page.click_comfort_tariff_button()
         comfort_tariff = self.driver.find_elements(*self.routes_page.comfort_tariff_button)
         assert "tcard" in self.driver.find_element(*urban_routes_page.UrbanRoutesPage.comfort_tariff_button).get_attribute("class")
         assert comfort_tariff[4].is_enabled()
 
     def test_fill_phone_number(self):
-        self.routes_page.setup_route(data.address_from, data.address_to)
-        self.routes_page.click_phone_number_field()
-        self.routes_page.fill_in_phone_number()
-        self.routes_page.click_next_button()
-        code = helpers.retrieve_phone_code(self.driver)
-        self.routes_page.set_confirmation_code(code)
-        self.routes_page.click_code_confirmation_button()
+        self.routes_page.setup_route()
+        self.routes_page.enter_full_phone_number_and_confirm()
         phone_input_value = self.driver.find_element(*self.routes_page.phone_input).get_attribute("value")
         assert phone_input_value == data.phone_number
 
     def test_add_credit_card(self):
-        self.routes_page.setup_route(data.address_from, data.address_to)
-        self.routes_page.click_payment_method_field()
-        self.routes_page.click_add_card_button()
-        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(self.routes_page.card_number_field)).send_keys(data.card_number)
-        self.routes_page.enter_card_number()
-        self.routes_page.enter_card_code()
-        self.routes_page.press_tab_key()
-        self.routes_page.click_add_button()
+        self.routes_page.setup_route()
+        self.routes_page.add_credit_card()
         card_input = self.driver.find_elements(*self.routes_page.card_added)[1]
         assert card_input.is_enabled()
-        self.routes_page.click_card_close_button()
 
     def test_write_message(self):
-        self.routes_page.setup_route(data.address_from, data.address_to)
+        self.routes_page.setup_route()
         self.routes_page.enter_new_message()
         assert self.driver.find_element(*self.routes_page.message).get_property('value') == data.message_for_driver
 
     def test_request_blanket_and_scarves(self):
-        self.routes_page.setup_route(data.address_from, data.address_to)
+        self.routes_page.setup_route()
         self.routes_page.click_comfort_tariff_button()
         self.routes_page.click_blanket_and_scarves_switch()
         checkbox = self.driver.find_element(*urban_routes_page.UrbanRoutesPage.switch_checkbox)
         assert checkbox.is_selected()
 
     def test_request_icecream(self):
-        self.routes_page.setup_route(data.address_from, data.address_to)
+        self.routes_page.setup_route()
         self.routes_page.click_comfort_tariff_button()
         self.routes_page.click_add_icecream()
         self.routes_page.click_add_icecream()
@@ -76,22 +64,9 @@ class TestUrbanRoutes:
         assert icecream_count == 2
 
     def test_search_taxi(self):
-        self.routes_page.setup_route(data.address_from, data.address_to)
-        self.routes_page.click_comfort_tariff_button()
-        self.routes_page.click_phone_number_field()
-        self.routes_page.fill_in_phone_number()
-        self.routes_page.click_next_button()
-        code = helpers.retrieve_phone_code(self.driver)
-        self.routes_page.set_confirmation_code(code)
-        self.routes_page.click_code_confirmation_button()
-        self.routes_page.click_payment_method_field()
-        self.routes_page.click_add_card_button()
-        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(self.routes_page.card_number_field)).send_keys(data.card_number)
-        self.routes_page.enter_card_number()
-        self.routes_page.enter_card_code()
-        self.routes_page.press_tab_key()
-        self.routes_page.click_add_button()
-        self.routes_page.click_card_close_button()
+        self.routes_page.setup_route()
+        self.routes_page.enter_full_phone_number_and_confirm()
+        self.routes_page.add_credit_card()
         self.routes_page.enter_new_message()
         self.routes_page.click_blanket_and_scarves_switch()
         self.routes_page.click_add_icecream()
