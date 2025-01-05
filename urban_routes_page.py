ffa6f0bafb7fbb4b9d21122cb1ad6083882decf1
angelguerra1999 <134436845+angelguerra1999@users.noreply.github.com>
2025-01-05 13:36:42 -0300

    Update urban_routes_page.py

--- urban_routes_page.py
+++ urban_routes_page.py
@@ -3,45 +3,17 @@ from selenium.webdriver import Keys
 import data
 
 class UrbanRoutesPage:
-    from_field = (By.ID, 'from')
-    to_field = (By.ID, 'to')
-    order_taxi_button = (By.XPATH, '//*[contains(text(), "Pedir un taxi")]')
-    comfort_tariff_button = (By.CLASS_NAME, 'tcard')
-    telephone_number = (By.CLASS_NAME, "np-button")
-    phone_input = (By.ID, 'phone')
-    next_button = (By.XPATH, '//*[contains(text(), "Siguiente")]')
-    code_field = (By.ID, 'code')
-    code_confirmation_button = (By.XPATH, '//*[contains(text(), "Confirmar")]')
-    payment_method = (By.CLASS_NAME, "pp-button")
-    add_card = (By.XPATH, '//*[contains(text(), "Agregar tarjeta")]')
-    card_added = (By.CLASS_NAME, 'pp-row')
-    card_number_field = (By.NAME, 'number')
-    card_code_field = (By.NAME, 'code')
-    add_button = (By.XPATH, '//button[text()="Agregar"]')
-    card_close_button = (By.CSS_SELECTOR, '.payment-picker.open .modal .section.active .close-button')
-    message = (By.ID, 'comment')
-    blanket_and_scarves_switch = (By.CLASS_NAME, "switch")
-    add_icecream = (By.CLASS_NAME, "counter-plus")
-    order_a_taxi = (By.CLASS_NAME, "smart-button-wrapper")
-    modal_opcional = (By.XPATH, '//*[contains(text(), "El conductor llegará en")]')
-    switch_checkbox = (By.CLASS_NAME, "switch-input")
-    icecream_counter = (By.CLASS_NAME, "counter-value")
-
+    # Selectores...
+    
     def __init__(self, driver):
         self.driver = driver
 
     # Métodos de acciones en la página
     def set_from(self, from_address):
         self.driver.find_element(*self.from_field).send_keys(from_address)
-
+    
     def set_to(self, to_address):
         self.driver.find_element(*self.to_field).send_keys(to_address)
-
-    def get_from(self):
-        return self.driver.find_element(*self.from_field).get_property('value')
-
-    def get_to(self):
-        return self.driver.find_element(*self.to_field).get_property('value')
 
     def click_order_taxi_button(self):
         self.driver.find_element(*self.order_taxi_button).click()
@@ -55,13 +27,13 @@ class UrbanRoutesPage:
 
     def fill_in_phone_number(self):
         self.driver.find_element(*self.phone_input).send_keys(data.phone_number)
-
+    
     def click_next_button(self):
         self.driver.find_element(*self.next_button).click()
 
     def set_confirmation_code(self, code):
         self.driver.find_element(*self.code_field).send_keys(code)
-
+    
     def click_code_confirmation_button(self):
         self.driver.find_element(*self.code_confirmation_button).click()
 
@@ -97,11 +69,29 @@ class UrbanRoutesPage:
 
     def click_order_a_taxi(self):
         self.driver.find_element(*self.order_a_taxi).click()
-
+    
     def wait_opcional_modal(self):
         self.driver.find_element(*self.modal_opcional)
-
-    def setup_route(self, from_address, to_address):
-        self.set_from(from_address)
-        self.set_to(to_address)
+    
+    def setup_route(self):
+        self.set_from(data.address_from)
+        self.set_to(data.address_to)
         self.click_order_taxi_button()
+    
+    def enter_full_phone_number_and_confirm(self):
+        self.click_phone_number_field()
+        self.fill_in_phone_number()
+        self.click_next_button()
+        code = helpers.retrieve_phone_code(self.driver)
+        self.set_confirmation_code(code)
+        self.click_code_confirmation_button()
+    
+    def add_credit_card(self):
+        self.click_payment_method_field()
+        self.click_add_card_button()
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(self.card_number_field)).send_keys(data.card_number)
+        self.enter_card_number()
+        self.enter_card_code()
+        self.press_tab_key()
+        self.click_add_button()
+        self.click_card_close_button()
